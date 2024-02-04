c28730f35230ab0685d8983527f343c52b0eb00d
ENZOMOTIVE <aayushmanbhabapadhy@gmail.com>
2024-02-04 16:39:58 +0530

    update

--- README.md
+++
@@ -1,2 +0,0 @@
-# Patient_Data_Manager
- Software which uses Blockchain technology to store patient healthcare records
--- medicalrecord/scripts/deploy.js
+++ medicalrecord/scripts/deploy.js
@@ -5,8 +5,8 @@ async function main() {
   const account = await ethers.getSigners();
   const medical = await Medical.connect(account[1]).deploy();
 
-  medical.waitForDeployment();
-  console.log(`Medical is deployed in address ${medical.target}`);
+  await medical.deployed();
+  console.log(`Medical is deployed in address ${medical.address}`);
 }
 main()
   .then(() => process.exit(0))
