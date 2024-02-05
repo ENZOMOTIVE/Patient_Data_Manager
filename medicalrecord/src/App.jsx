84fe57febeec7aa10ab45ddcedc9bede3a2cc9ac
ENZOMOTIVE <aayushmanbhabapadhy@gmail.com>
2024-02-05 07:13:25 +0530

    Update App.jsx

--- medicalrecord/src/App.jsx
+++ medicalrecord/src/App.jsx
@@ -1,31 +1,19 @@
-import { useState } from 'react'
-
-
+import { useEffect } from "react";
+import { loadProvider } from "./store/interactions";
+import { useDispatch } from "react-redux";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const dispatch = useDispatch();
+  const loadblockchainData = async () => {
+    const provider = loadProvider(dispatch);
+    console.log(provider);
+  };
+  useEffect(() => {
+    loadblockchainData();
+  });
 
-  return (
-    <>
-      <div>
-      
-      
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return <div className="App"> The blockchain century</div>
 }
 
-export default App
+export default App;
