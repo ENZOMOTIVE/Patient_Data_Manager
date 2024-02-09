77bf58dcf76cc2fef71062b41cd34d6bbddbb065
ENZOMOTIVE <aayushmanbhabapadhy@gmail.com>
2024-02-09 23:35:19 +0530

    initial_commit

--- medicalrecord/src/components/Navbar/Navbar.js
+++ medicalrecord/src/components/Navbar/Navbar.js
@@ -1,79 +1,94 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./navbar.css";
 import healthReport from "../../assets/health-report.png";
-import { useDispatch, useSelector } from "react-redux";
 import { loadAccount } from "../../store/interactions";
+import { useDispatch, useSelector } from "react-redux";
 import Blockies from "react-blockies";
 import config from "../../config.json";
 
 const Navbar = () => {
   const dispatch = useDispatch();
-  const provider=useSelector((state)=>state.provider.connection);
+  const provider = useSelector((state) => state.provider.connection);
   const account = useSelector((state) => state.provider.account);
   const balance = useSelector((state) => state.provider.balance);
   const chainId = useSelector((state) => state.provider.chainId);
+  
+  const connectHandler = async () => {
+    await loadAccount(provider, dispatch);
+  };
 
-  const connectHandler=async()=> {
-    await loadAccount(provider,dispatch);
-  }
-  const networkHandler=async(e)=> {
+  const networkHandler = async (e) => {
     await window.ethereum.request({
-    method:"wallet_switchEthereumChain",
-    params: [
-      {
-        chainId: e.target.value,
-      },
-        ],
+      method: "wallet_switchEthereumChain",
+      params: [
+        {
+          chainId: `0x${e.target.value}`, // Prefix chainId with '0x'
+        },
+      ],
     });
   };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await loadAccount(provider, dispatch);
+    };
+    fetchData();
+  }, [provider, dispatch]);
+
   return (
     <div className="Navbar">
-        <div className="nav__name">
-            <img src={healthReport} width= "40" height= "40" alt=" healthrecord_image"/>
-                <h2>Medical Record Storer</h2>
-        </div>
-        <div className="nav__networkSelector">
-            <select name="network" id="network" onCharge={networkHandler} value={config[chainId] ? `0x${chainId.toString(16)}` : `0`}>
-                 <option value= "0" disabled>
-                    Select Network
-                 </option>
-                 <option value="31337"> Localhost</option>
-                 <option value="11155111"> Sepolia</option>
-            </select>
-        </div>
-
-        <div className="nav__balance">
-          {balance ? (
-            <p className="nav__myBalance">
-              <small>My Balance: </small>
-              {Number(balance).toFixed(4)}
-            </p>
-          ) : (
-            <p className="nav__myBalance">
-              <small>My Balance: </small>0 ETH
-            </p>
-          )}
-          {account ? (
-            <a className="nav__myAccount" href="#">
-              <Blockies 
-                seed={account}
-                size={10}
-                scale={3}
-                color="2187D0"
-                bgColor="#F1F2F9"
-                spotColor="#767f92"
-                className="identicon"
-                    />
-              {account.slice(0,5) + "...." + account.slice(38,42)}
-              
-            </a>
-          ):(
-            <button className="nav__balance-box" onClick={connectHandler}>Connect</button>
-          )}
-          
-        </div>
+      <div className="nav__name">
+        <img src={healthReport} alt="" width="40" height="40" />
+        <h2>Subham Medical Record </h2>
+      </div>
+      <div className="nav__networkSelector">
+        <select
+          name="network"
+          id="network"
+          onChange={networkHandler}
+          value={chainId} // Use plain chainId without manipulation
+        >
+           <option value="" disabled>
+            Select Network
+          </option>
+          <option value="31337">Localhost</option>
+          <option value="5">Goerli</option>
+          <option value="80001">Mumbai</option>
+        </select>
+      </div>
+      <div className="nav__balance">
+        {balance ? (
+          <p className="nav__myBalance">
+            <small>My Balance : </small>
+            {Number(balance).toFixed(4)}
+          </p>
+        ) : (
+          <p className="nav__myBalance">
+            <small>My Balance : </small>
+            0ETH
+          </p>
+        )}
+        {account ? (
+          /* eslint-disable jsx-a11y/anchor-is-valid */
+          <a className="nav__myAccount" href="#">
+            {account.slice(0, 5) + "...." + account.slice(38, 42)}
+            <Blockies
+              seed={account}
+              size={10}
+              scale={3}
+              color="#2187D0"
+              bgColor="#F1F2F9"
+              spotColor="#767F92"
+              className="identicon"
+            />
+          </a>
+        ) : (
+          <button className="nav__balance-box" onClick={connectHandler}>
+            Connect
+          </button>
+        )}
+      </div>
     </div>
-    
   );
 };
 
